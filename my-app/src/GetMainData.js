ff759d5229230b896ecc67a1bcefad4557e556d3
Andrew Berg <onlineussrbusiness@gmail.com>
2023-02-03 14:48:24 +0000

    Changed Looks, Fixed Bugs
    - Changed how to table body looks in order to resolve hover issues.
    - Restructured the code with regards to the table so it would be easier to code and or read.
    - Removed redundant code/styles

--- my-app/src/GetMainData.js
+++ my-app/src/GetMainData.js
@@ -1,119 +1,36 @@
 import "./styles/GetMainData.css"
 
 function GetMainData({data, id, dateOptions}) {
-    function hideAccountDetails() {
-        if (document.getElementById("mainContent").style.display === "none") {
-            document.getElementById("mainContent").style.display = "contents"
+    function toggleMainDetails() {
+        if (document.getElementById("mainRows").style.display === "none") {
+            document.getElementById("mainRows").style.display = "contents"
         }
 
         else {
-            document.getElementById("mainContent").style.display = "none"
+            document.getElementById("mainRows").style.display = "none"
         }
     }
 
     return (
         <table id="mainDataTable">
             <thead>
-                <tr id="mainTitle">
+                <tr id="mainDataTableTitle">
                     <th colSpan="2">
-                        <button onClick={hideAccountDetails}>Account Details</button>
+                        <button onClick={toggleMainDetails}>Account Details</button>
                     </th>
                 </tr>
-                <tr id="mainContent">
-                    <td>
-                        <table>
-                            <thead>
-                                <tr id="nickName">
-                                    <td>Nickname</td>
-                                    <td>{data.data[id].nickname}</td>
-                                </tr>
-                                <tr id="accountID">
-                                    <td>Account ID</td>
-                                    <td>{data.data[id].account_id}</td>
-                                </tr>
-                                <tr id="lastBattle">
-                                    <td>Last Battle</td>
-                                    <td>
-                                        {new Date(data.data[id].last_battle_time * 1000).toLocaleString("en-IE", dateOptions)}<br/>
-                                        {new Date(data.data[id].last_battle_time * 1000).toLocaleTimeString("en-IE")}
-                                    </td>
-                                </tr>
-                                <tr id="logoutAt">
-                                    <td>Logged Out</td>
-                                    <td>
-                                        {new Date(data.data[id].logout_at * 1000).toLocaleString("en-IE", dateOptions)}<br/>
-                                        {new Date(data.data[id].logout_at * 1000).toLocaleTimeString("en-IE")}
-                                    </td>
-                                </tr>
-                                <tr id="createdAt">
-                                    <td>Registration Date</td>
-                                    <td>
-                                        {new Date(data.data[id].created_at * 1000).toLocaleString("en-IE", dateOptions)}<br/>
-                                        {new Date(data.data[id].created_at * 1000).toLocaleTimeString("en-IE")}
-                                    </td>
-                                </tr>
-                                <tr id="updatedAt">
-                                    <td>Profile Updated</td>
-                                    <td>
-                                        {new Date(data.data[id].updated_at * 1000).toLocaleString("en-IE", dateOptions)}<br/>
-                                        {new Date(data.data[id].updated_at * 1000).toLocaleTimeString("en-IE")}
-                                    </td>
-                                </tr>
-                                <tr id="globalRating">
-                                    <td>Global Rating</td>
-                                    <td>{data.data[id].global_rating}</td>
-                                </tr>
-                                <tr id="clanID">
-                                    <td>Clan ID</td>
-                                    <td>{data.data[id].clan_id === null ? <i>NaN</i> : data.data[id].clan_id}</td>
-                                </tr>
-                            </thead>
-                        </table>
-                    </td>
-                    {/* <td id="mainSectionRight">
-                        <table>
-                            <thead>
-                                <tr id="nickNameValue">
-                                    <td>{data.data[id].nickname}</td>
-                                </tr>
-                                <tr id="accountIDValue">
-                                    <td>{data.data[id].account_id}</td>
-                                </tr>
-                                <tr id="lastBattleValue">
-                                    <td>
-                                        {new Date(data.data[id].last_battle_time * 1000).toLocaleString("en-IE", dateOptions)}<br/>
-                                        {new Date(data.data[id].last_battle_time * 1000).toLocaleTimeString("en-IE")}
-                                    </td>
-                                </tr>
-                                <tr id="logoutAtValue">
-                                    <td>
-                                        {new Date(data.data[id].logout_at * 1000).toLocaleString("en-IE", dateOptions)}<br/>
-                                        {new Date(data.data[id].logout_at * 1000).toLocaleTimeString("en-IE")}
-                                    </td>
-                                </tr>
-                                <tr id="createdAtValue">
-                                    <td>
-                                        {new Date(data.data[id].created_at * 1000).toLocaleString("en-IE", dateOptions)}<br/>
-                                        {new Date(data.data[id].created_at * 1000).toLocaleTimeString("en-IE")}
-                                    </td>
-                                </tr>
-                                <tr id="updatedAtValue">
-                                    <td>
-                                        {new Date(data.data[id].updated_at * 1000).toLocaleString("en-IE", dateOptions)}<br/>
-                                        {new Date(data.data[id].updated_at * 1000).toLocaleTimeString("en-IE")}
-                                    </td>
-                                </tr>
-                                <tr id="globalRatingValue">
-                                    <td>{data.data[id].global_rating}</td>
-                                </tr>
-                                <tr id="clanIDValue">
-                                    <td>{data.data[id].clan_id === null ? <i>NaN</i> : data.data[id].clan_id}</td>
-                                </tr>
-                            </thead>
-                        </table>
-                    </td> */}
-                </tr>
             </thead>
+
+            <tbody id="mainRows">
+                <tr id="mainRow"><td>Nickname</td><td>{data.data[id].nickname}</td></tr>
+                <tr id="mainRow"><td>Account ID</td><td>{data.data[id].account_id}</td></tr>
+                <tr id="mainRow"><td>Last Battle</td><td>{new Date(data.data[id].last_battle_time * 1000).toLocaleString("en-IE", dateOptions)} <br/> {new Date(data.data[id].last_battle_time * 1000).toLocaleTimeString("en-IE")}</td></tr>
+                <tr id="mainRow"><td>Logged Out</td><td>{new Date(data.data[id].logout_at * 1000).toLocaleString("en-IE", dateOptions)} <br/> {new Date(data.data[id].logout_at * 1000).toLocaleTimeString("en-IE")}</td></tr>
+                <tr id="mainRow"><td>Registration Date</td><td>{new Date(data.data[id].created_at * 1000).toLocaleString("en-IE", dateOptions)} <br/> {new Date(data.data[id].created_at * 1000).toLocaleTimeString("en-IE")}</td></tr>
+                <tr id="mainRow"><td>Last Profile Update</td><td>{new Date(data.data[id].updated_at * 1000).toLocaleString("en-IE", dateOptions)} <br/> {new Date(data.data[id].updated_at * 1000).toLocaleTimeString("en-IE")}</td></tr>
+                <tr id="mainRow"><td>Global Rating</td><td>{data.data[id].global_rating}</td></tr>
+                <tr id="mainRow"><td>Clas ID</td><td>{data.data[id].clan_id === null ? <i>NaN</i> : data.data[id].clan_id}</td></tr>
+            </tbody>
         </table>
     )
 }
--- my-app/src/GetRandomData.js
+++ my-app/src/GetRandomData.js
@@ -2,75 +2,39 @@ import "./styles/GetRandomData.css"
 
 function GetRandomData({data, id}) {
     function toggleRandomDetails() {
-        if (document.getElementById("randomContent").style.display === "none") {
-            document.getElementById("randomContent").style.display = "contents"
+        if (document.getElementById("randomRows").style.display === "none") {
+            document.getElementById("randomRows").style.display = "table-row-group"
         }
 
         else {
-            document.getElementById("randomContent").style.display = "none"
+            document.getElementById("randomRows").style.display = "none"
         }
     }
 
-    // function toggleRandomBattle_Battles() {
-    //     for (var i = 0; i < document.getElementsByClassName("randomBattle_Battles").length; i++) {
-    //         if (document.getElementsByClassName("randomBattle_Battles")[i].style.display === "none") {
-    //             document.getElementsByClassName("randomBattle_Battles")[i].style.display = "table-row"
-    //         }
-    
-    //         else {
-    //             document.getElementsByClassName("randomBattle_Battles")[i].style.display = "none"
-    //         }
-    //     }
-    // }
-
-    var item = data.data[id].statistics.random;
+    var randomDataStats = data.data[id].statistics.random;
 
     return (
         <table id="randomDataTable">
             <thead>
-                <tr id="randomTitle">
-                    <th colSpan="2"><button onClick={toggleRandomDetails}>Random Battle</button></th>
-                </tr>
-                <tr id="randomContent">
-                    <td id="randomSectionLeft">
-                        <table>
-                            <thead>
-                                <tr id="battles" className="randomBattle_Battles"><td>Battles</td></tr>
-                                <tr id="wins" className="randomBattle_Battles"><td>Wins</td></tr>
-                                <tr id="losses" className="randomBattle_Battles"><td>Losses</td></tr>
-                                <tr id="draws" className="randomBattle_Battles"><td>Draws</td></tr>
-                                <tr id="survivedBattles" className="randomBattle_Battles"><td>Battles Survived</td></tr>
-
-                                <tr id="xp"><td>Total Exp.</td></tr>
-                                <tr id="avgBattleXP"><td>Average Exp.</td></tr>
-
-                                <tr id="frags"><td>Frags</td></tr>
-                                <tr id="hits"><td>Hits</td></tr>
-                                <tr id="hitsPercentage"><td>Accuracy</td></tr>
-                            </thead>
-                        </table>
-                    </td>
-
-                    <td id="randomSectionRight">
-                        <table>
-                            <thead>
-                                <tr id="battlesValue" className="randomBattle_Battles"><td>{Number(item.battles).toLocaleString()}</td></tr>
-                                <tr id="winsValue" className="randomBattle_Battles"><td>{Number(item.wins).toLocaleString()}</td></tr>
-                                <tr id="lossesValue" className="randomBattle_Battles"><td>{Number(item.losses).toLocaleString()}</td></tr>
-                                <tr id="drawsValue" className="randomBattle_Battles"><td>{Number(item.draws).toLocaleString()}</td></tr>    
-                                <tr id="survivedBattlesValue" className="randomBattle_Battles"><td>{item.draws}</td></tr>
-
-                                <tr id="xpValue"><td>{Number(item.xp).toLocaleString()}</td></tr>
-                                <tr id="avgBattleXPValue"><td>{Number(item.battle_avg_xp).toLocaleString()}</td></tr>
-
-                                <tr id="fragsValue"><td>{Number(item.frags).toLocaleString()}</td></tr>
-                                <tr id="hitsValue"><td>{Number(item.hits).toLocaleString()}</td></tr>
-                                <tr id="hitsPercentageValue"><td>{item.hits_percents + "%"}</td></tr>
-                            </thead>
-                        </table>
-                    </td>
+                <tr id="randomDataTableTitle">
+                    <th colSpan="2">
+                        <button onClick={toggleRandomDetails}>Random Battle</button>
+                    </th>
                 </tr>
             </thead>
+
+            <tbody id="randomRows">
+                <tr id="randomRow"><td>Battles</td><td>{Number(randomDataStats.battles).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Wins</td><td>{Number(randomDataStats.wins).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Losses</td><td>{Number(randomDataStats.losses).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Draws</td><td>{Number(randomDataStats.draws).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Battles Survived</td><td>{Number(randomDataStats.survived_battles).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Total Experience</td><td>{Number(randomDataStats.xp).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Average Experience</td><td>{Number(randomDataStats.battle_avg_xp).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Frags</td><td>{Number(randomDataStats.frags).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Hits</td><td>{Number(randomDataStats.hits).toLocaleString()}</td></tr>
+                <tr id="randomRow"><td>Accuracy</td><td>{Number(randomDataStats.hits_percents).toLocaleString() + "%"}</td></tr>
+            </tbody>
         </table>
     )
 }
