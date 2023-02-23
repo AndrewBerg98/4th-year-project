f3df76d8f2054fc482a7697f47a534a7bb1fd54a
Andrew Berg <onlineussrbusiness@gmail.com>
2023-02-23 09:25:34 +0000

    IP Value Toggle Option
    - IP value is hidden initially when error appears, until clicked on.

--- my-app/src/GetPlayer.js
+++ my-app/src/GetPlayer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 import GetPersonalData from "./GetPersonalData"
+import "./styles/GetPlayer.css"
 
 function GetPlayer({nickname, realm, urlPlayerList, source}) {
     const [data, setData] = useState(null)
@@ -29,6 +30,18 @@ function GetPlayer({nickname, realm, urlPlayerList, source}) {
         }
     }, [nickname, urlPlayerList, source])
 
+    function ToggleIP() {
+        if (document.getElementById("errorValueActual").style.display === "none") {
+            document.getElementById("errorValue").style.display = "none"
+            document.getElementById("errorValueActual").style.display = "inline-block"
+        }
+
+        else if (document.getElementById("errorValueActual").style.display !== "none") {
+            document.getElementById("errorValue").style.display = "inline-block"
+            document.getElementById("errorValueActual").style.display = "none"
+        }
+    }
+
     if (nickname !== undefined && data !== null) {
         var match = false
 
@@ -39,7 +52,8 @@ function GetPlayer({nickname, realm, urlPlayerList, source}) {
                     <h4>Code: <span id="errorCode">{data.error.code}</span></h4>
                     <h4>Message: <span id="errorMessage">{data.error.message}</span></h4>
                     <h4>Field: <span id="errorField">{data.error.field}</span></h4>
-                    <h4>Value: <span id="errorValue">{data.error.value}</span></h4>
+                    <h4>Value: <span id="errorValue" onClick={() => ToggleIP()} style={{display: "inline-block"}}><i>Hover to Show Your IP</i></span><span id="errorValueActual" onClick={() => ToggleIP()} style={{display: "none"}}>{data.error.value}</span>
+                    </h4>
                     <p>You may need to update the IP Address List on the WarGaming Developer site:</p>
                     <a href="https://developers.wargaming.net/applications/" target="_blank" rel="noopener noreferrer">https://developers.wargaming.net/applications/</a>
                 </div>
