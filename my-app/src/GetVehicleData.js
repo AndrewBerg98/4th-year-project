6c54fc3a243005e49707899b999f1f1ec550c661
Andrew Berg <onlineussrbusiness@gmail.com>
2023-05-09 03:24:06 +0100

    Disabled Tooltip for Tree map
    - Disabled tooltip for tree map chart as it causes an error for new users.

--- my-app/src/GetVehicleData.js
+++ my-app/src/GetVehicleData.js
@@ -704,7 +704,7 @@ function GetVehicleData({id, realm, source, totalBattles}) {
                     <ResponsiveContainer width="100%" height="100%">
                         <Treemap width={400} height={200} data={vehicleHaves} dataKey="size" aspectRatio={4 / 3} stroke="#fff" fill="#8884d8"
                         content={<CustomizedTreeMapContent colors={COLORS} />} isAnimationActive={false}>
-                            <Tooltip content={<TreeMapChartCustomTooltip />}  isAnimationActive={false} />
+                            {/* <Tooltip content={<TreeMapChartCustomTooltip />}  isAnimationActive={false} /> */}
                         </Treemap>
                     </ResponsiveContainer>
                 </div>
